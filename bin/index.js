f6dfa2ace4a901d9fcc5f9d6a7ca97866a3e57d4
Jose Augusto Queiroz Comparotto Gomes <jose.augusto.comparotto@gmail.com>
2020-08-25 09:50:20 -0400

    menu

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 const yargs = require("yargs")
 
+const argv = yargs
+    .usage("Usage: $0 [options] [--type <type>] <value> <from> <to>")
+    .help()
+    .version()
+    .option("type", {
+        type: "string",
+        describe: "The type of value (ex: length, area, volume, etc.),\nif not given, it will be recognized automatically."
+    })
+    .option("value", {
+        type: "number",
+        describe: "The input value.",
+        alias: "v"
+    })
+    .option("from", {
+        type: "string",
+        describe: "The unit of the input value.",
+        alias: "f"
+    })
+    .option("to", {
+        type: "string",
+        describe: "The output unit.",
+        alias: "t"
+    })
+    .argv
+
+console.warn("Not avaliable yet );")
